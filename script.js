91295d092f6de02ffa86f61ed1fc2b492b632a94
Mustafa Guer <mustafaguer@outlook.com>
2021-09-06 05:36:54 +0200

    datalist

--- script.js
+++ script.js
@@ -7,28 +7,24 @@ async function findCity() {
 
     let city = document.getElementById('city');
     city.value = responseAsJson['city'];
-        
-    console.log('response', responseAsJson);
+
 }
 
 async function findStreet(plz) {
     console.log(plz);
     let street = document.getElementById('street');
     let search = street.value.toLowerCase();
-    search.split(0, 1);
-    console.log('search', search[0])
-    // let response = await fetch(URL_Server + 'autocomplete=street' + '&' + `plz_plz=${plz}` + '&' + `street=${street.value}`);
-    // let responseAsJson = await response.json();
 
     let response = await fetch(URL_Server + 'finda=streets' + '&' + `plz_plz=${plz}`);
     let responseAsJson = await response.json();
     let streets = responseAsJson['rows'];
 
-    streets.forEach(s => {
-        if(s['street'].toLowerCase()[0] == (search[0])) {
-            console.log('s', s);
-        }
-    });
+    document.getElementById('streets').innerHTML = '';
 
-    // console.log('streetjson', streets);
+    streets.forEach(s => {
+
+        if (s['street'].toLowerCase().includes(search)) {
+            document.getElementById('streets').innerHTML += `<option value="${s['street']}">`;
+        }
+    })
 }
